dd93ea54ad72500a3bf09244eec9a8c72633a653
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-01-05 11:51:57 +0530

    Few components with props added

--- hooksinreact/src/App.jsx
+++ hooksinreact/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+import Card2 from "./components/Card2";
 
+let data = {
+  name: ["Vivek", "Divya", "Bindi", "Chotu"],
+  specs: [
+    { vivek: "Vivek got a pakage of 35 Lpa in jan 24" },
+    { Divya: "Divya will come to prayagraj on 1st Feb 24" },
+    { Bindi: "Bindi loves chotu" },
+    { Chotu: "Chotu loves bindi" },
+  ],
+};
 function Appp() {
-
+  
   return (
     <>
-      <div class="w-[300px] rounded-md border">
-  <img
-    src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-    alt="Laptop"
-    class="h-[200px] w-full rounded-md object-cover"
-  />
-  <div class="p-4">
-    <h1 class="text-lg font-semibold">About Macbook</h1>
-    <p class="mt-3 text-sm text-gray-600">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
-    </p>
-    <button
-      type="button"
-      class="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-    >
-      Read
-    </button>
-  </div>
-</div>
-
+      <h1>Vivek got a pakage of 35 Lpa in Jan 24</h1>
+      <Card name = {data.name[0]} specs = {data.specs[0].vivek} />
+      <Card2 name = {data.name[1]} specs = {data.specs[1].Divya}/>
+      <Card name = {data.name[2]} specs = {data.specs[2].Bindi}/>
+      <Card name = {data.name[3]} specs = {data.specs[3].Chotu}/>
     </>
-  )
+  );
 }
 
-export default Appp
+export default Appp;
